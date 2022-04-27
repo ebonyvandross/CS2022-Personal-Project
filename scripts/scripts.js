bc379a0e5168fcc8ee998b1e4accaabd5877ea77
ebonyvandross <94580385+ebonyvandross@users.noreply.github.com>
2022-04-26 22:51:39 -0400

    Added updated files to repo on 4.26.22

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+console.log("SCRIPTS FOR PERSONAL PROJECT");
+
+// For responsive mobile nav menu
+// 1 Select hamburger menu icon
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+
+// 2 Select nav list
+const navList = document.querySelector("#nav-list");
+
+// 3 Add event listener for hamburger icon
+hamburgerIcon.addEventListener("click", () => {
+  navList.classList.toggle("open")
+})
+
+// Write function that will show nav menu after clicking hamburger icon
+function shownavMenu() {
+  if hamburgerIcon 
+}
