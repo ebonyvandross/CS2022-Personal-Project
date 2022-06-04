25cc59a5204b772016ea6fd88c5446bcdcfd0857
ebonyvandross <94580385+ebonyvandross@users.noreply.github.com>
2022-06-03 23:09:03 -0400

    Added updates

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 mongoose.connect("process.env.DB_URL", {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
